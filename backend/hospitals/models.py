171a80b911a8a49e731967e94cb04543a3221785
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-03-19 12:25:11 +0300

    Setup hospital model.py

--- backend/hospitals/models.py
+++ backend/hospitals/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Hospital(models.Model):
+    name=models.CharField(max_length=300)
+    location=models.CharField(max_length=280)
+    services=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
