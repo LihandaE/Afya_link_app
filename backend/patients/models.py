46f28bfb1d943d8f25cbcbdc4d9d999bff7ea817
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-03-19 13:03:12 +0300

    Set  up patients models.py

--- backend/patients/models.py
+++ backend/patients/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Patient(models.Model):
+    GENDER_CHOICES=(
+        ('male','Male'),
+        ('female','Female'),
+        ('other','Other'),
+    )
+    full_name=models.CharField(max_length=280)
+    phone=models.CharField(max_length=25)
+    email=models.EmailField(blank=True, null=True)
+    date_of_birth=models.DateField()
+    gender=models.CharField(max_length=10, choices=GENDER_CHOICES)
+    national_id=models.CharField(max_length=50, unique=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
