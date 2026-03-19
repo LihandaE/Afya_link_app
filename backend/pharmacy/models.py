5a40cbec5f84c0ae65b73d9ebb531a212af8897c
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-03-19 16:28:41 +0300

    Set  up pharmacy models.py

--- backend/pharmacy/models.py
+++ backend/pharmacy/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+class Prescription(models.Model) :
+    visit=models.ForeignKey(
+        'visits.Visit',
+        on_delete=models.CASCADE
+
+    )
+    doctor=models.ForeignKey(
+        'accounts.User',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    medication=models.CharField(max_length=255)
+    dosage=models.CharField(max_length=260)
+    instructions=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Prescription for {self.visit.patient.full_name}"
+
+
+#Pharmacy Dispensing
+    
+class DispenseMedication(models.Model):
+    prescription=models.OneToOneField(
+        Prescription,
+        on_delete=models.CASCADE
+
+    )
+    pharmacist=models.ForeignKey(
+        'accounts.User',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    dispensed_at=models.DateTimeField(auto_now_add=True)
+
+    status=models.CharField(
+        max_length=50,
+        default='dispensed'
+
+    )
+    def __str__(self):
+        return f"Dispensed for{self.prescription.visit.patient.full_name}"
