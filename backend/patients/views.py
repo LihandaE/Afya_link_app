31077067420f6bde5b6d6a12eef929f94a4c52fb
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-03-20 12:33:17 +0300

    Set up patients view and patients serializers under patients app

--- backend/patients/views.py
+++ backend/patients/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view,permission_classes
+from .models import Patient
+from  accounts.permissions import IsReceptionist
+from .serializers import PatientSerializer
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view({'POST'})
+@permission_classes({IsReceptionist})
+def register_patient(request):
+    serializer= PatientSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
