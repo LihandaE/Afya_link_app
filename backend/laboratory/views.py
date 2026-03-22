41d573699d8216c60aecbebbd9d8359b751d38fa
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-03-22 19:35:00 +0300

    Setup Labaratory views.py and serializers.py

--- backend/laboratory/views.py
+++ backend/laboratory/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from accounts.permissions import IsLabTech
+from rest_framework.decorators import *
+from rest_framework.response import Response
+from .serializers import LabTestSerializer
 # Create your views here.
+
+@api_view(['POST'])
+@permission_classes([IsLabTech])
+def upload_lab_result(request):
+    serializer=LabTestSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(lab_technician=request.user)
+        return Response(serializer.data)
+    return Response(serializer.errors)
