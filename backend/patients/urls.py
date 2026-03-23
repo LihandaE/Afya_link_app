b27357fa5c8440cc68c6f2110ce4e03c5c506bd8
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-03-23 17:00:09 +0300

    Set up patients urls.py

--- backend/patients/urls.py
+++ backend/patients/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import register_patient, search_patient
+from .views import (register_patient,search_patient,patient_history)
 
-urlpatterns={
-    path('register/', register_patient),
-    path('search/' , search_patient),
-}
+urlpatterns = [
+
+    # Register patient (Receptionist)
+    path("register/", register_patient, name="register_patient"),
+
+    # Search patient (Receptionist, Doctor)
+    path("search/", search_patient, name="search_patient"),
+
+    # Patient full medical history (OTP protected)
+    path("history/<int:patient_id>/", patient_history, name="patient_history"),
+
+]
